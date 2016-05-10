69f21410cdc03f2bad8e13d9d3dc32909980d77d
bushuai <ibushuai@gmail.com>
2016-05-11 01:22:15 +0800

    config gulp & modify appearance

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass')
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer')
 
-
-var wacthFiles = [
-    "web/static/scss/**/*.scss",
-    "web/static/js/**/*.js"
-]
-
-gulp.task('styles', function() {
-    return gulp.src('./web/static/scss/**/*.scss')
+gulp.task('stylesheets', function() {
+    return gulp
+        .src('./web/static/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compact'
         }))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./web/static/css/'))
 })
 
@@ -28,9 +28,8 @@ gulp.task('serve', function() {
 })
 
 gulp.task('watch', function() {
-    gulp.watch(wacthFiles.scss, ['stylesheet'])
-    gulp.watch(wacthFiles.js, ['javascript'])
+    gulp.watch('web/static/scss/**/*.scss', ['stylesheets'])
 
 })
 
-gulp.task('default', ['styles'])
+gulp.task('default', ['watch', 'stylesheets'])
